a436c900a3b938e8ee9a1d506a40b782ec50561c
Cuba49 <kubovych.kostya59@gmail.com>
2017-07-25 19:35:19 +0300

    Delete local DB

--- Company/Company/App_Data/dbo.Company.sql
+++
@@ -1,8 +0,0 @@
-﻿CREATE TABLE [dbo].[Company] (
-    [Id]        INT           IDENTITY (1, 1) NOT NULL,
-    [Name]      NVARCHAR (50) NULL,
-    [FK_Parent] INT           NULL DEFAULT 0,
-    [MyValue]   INT           NULL,
-    PRIMARY KEY CLUSTERED ([Id] ASC)
-);
-
